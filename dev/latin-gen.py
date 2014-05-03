346eca30ad9d684ffec63ced3a0292d4f85fa870
Aboobacker MK <aboobackervyd@gmail.com>
2014-05-03 17:32:29 +0000

    added a script to add translitration as comment

---
+++ dev/latin-gen.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import sys
+from xml.dom import minidom
+import subprocess
+
+xmldoc = minidom.parse(sys.argv[1])
+itemlist = xmldoc.getElementsByTagName('e')
+for item in itemlist:
+	p=item.childNodes[1]
+	l=p.childNodes[0]
+	#print(l.toxml())
+	if l.firstChild :
+		mlword=l.firstChild.nodeValue
+		#print(mlword)
+		uconv = subprocess.Popen("uconv -x latn" , shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+		uconv.stdin.write(mlword.encode("utf-8"))
+		result = uconv.communicate()
+		result=result[0].decode("utf-8")
+		#print(result)
+		attr = xmldoc.createAttribute('c')
+		attr.value = result
+		item.setAttributeNode(attr)
+		try:
+			f= open(sys.argv[2], 'w')
+		except:
+			f= open('output.dix', 'w')
+		xmldoc.writexml(f, encoding= 'utf-8')
+		f.close()
+	else:
+		pass
+		
+
+
+
+
+
+
+
+
